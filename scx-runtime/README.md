490da9b6c91bb7423ea8fbaeb584d12ffdf56f36
DEVMICRO.APP <canna.seed.us@gmail.com>
2025-11-13 12:29:44 -0700

    Delete scx-runtime directory

--- scx-runtime/README.md
+++
@@ -1,2 +0,0 @@
-# SCX Runtime (Outboard)
-Drop-in loader and templates for external SCX until we embed it into MICRO.ASXR.
--- scx-runtime/scx.loader.js
+++
@@ -1,2 +0,0 @@
-// Outboard SCX loader. Swap this with your actual SCX runtime when ready.
-export default { mount(){}, compile(){}, info:'SCX outboard mode' };
